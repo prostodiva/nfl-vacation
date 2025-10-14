4da0405fd07ffb2d18c42c30526a86b90e7af9c4
prostodiva <rkattsyna@mail.ru>
2025-10-14 09:33:46 -0700

    refactor; add Root and depndencies for frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>My Header</div>;
+}
+
+export default Header;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>My Home Page</div>;
+}
+
+export default HomePage;
