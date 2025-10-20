e90018e897bc98f99ef5992fca01783c735973c1
prostodiva <rkattsyna@mail.ru>
2025-10-20 11:08:32 -0700

    Add: handleClick

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,16 +2,21 @@ import Button from "./Button";
 import Input from "./Input";
 
 function Hero() {
+
+const handleClick = () => {
+    console.log("Button clicked!");
+}
+
     return (
         <div>
-            <h1>Reusable components test</h1>
+            <h1>Hero Section: Reusable components test</h1>
             <Input />
             <Button 
                 primary
                 rounded
-                onClick={() => alert('Button clicked!')}
+                onClick={handleClick}
             >
-                Submit
+                List all Teams
             </Button>
         </div>
         
