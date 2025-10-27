ae34380f9fb8d1925c0237688235c52d51638c6e
prostodiva <rkattsyna@mail.ru>
2025-10-26 20:52:30 -0700

    Fix: width of the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
           <div className="overflow-x-auto whitespace-nowrap">
               <p className="text-[180px] leading-none text-black" style={{ fontFamily: 'SCHABO, sans-serif'}}>DALLAS COWBOYS · GREEN BAY PACKERS · KANSAS CITY CHIEFS · SAN FRANCISCO 49ERS · BUFFALO BILLS · MIAMI DOLPHINS · SEATTLE SEAHAWKS · CHICAGO BEARS · NEW YORK GIANTS · LAS VEGAS RAIDERS</p>
           </div>
-          <div className="w-480 h-[281px] pt-[126px] pr-[25px] pb-[27px] pl-[19px] bg-[#000]">
+          <div className="w-full h-[281px] pt-[126px] pr-[25px] pb-[27px] pl-[19px] bg-[#000]">
               <p className="text-white font-stretch-condensed text-7xl text-center">KEEP WATCHING THE GAME</p>
           </div>
       </div>
